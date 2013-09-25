dc74e37bb634f6db1b5cdc60256edaab3acdd4f1
Keith <keith@moran-obrien.com>
2013-09-24 22:50:20 -0400

    not working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,21 @@
 var rssApp = angular.module('rssApp', []);
 
+rssApp.directive('feed', function () {
+    return{
+        restrict: 'E',
+        replace: true,
+        //scope: {feed: '=cnnFeed'},
+        controller: function ($scope, $element, $attrs, Data) {
+            var feed_name = $attrs.site;
+            $scope.feed = Data.callUrl($attrs.site, $attrs.articleTag);
+        },
+        template: '<div class="pure-u-1-4 l-box header" > <p ng-repeat="item in feed">{{ item.title }}</p> </div> '
+    }
+});
+
 rssApp.factory('Data', function ($http) {
     $http.defaults.useXDomain = true;
-    function callUrl(url, articleTag) {
+    function getFeed(url, articleTag) {
 
         return $http.get(url)
             .then(function (result) {
@@ -20,17 +33,21 @@ rssApp.factory('Data', function ($http) {
     }
 
     return {
+
+        callUrl: function (feed_info) {
+            getFeed(feed_info.url, feed_info.article_tag === undefined ? 'item' : feed_info.article_tag);
+        },
         getCnn: function () {
             var url = 'http://rss.cnn.com/rss/cnn_topstories.rss';
-            return callUrl(url, 'item');
+            return getFeed(url, 'item');
         },
         getVerge: function () {
             var url = 'http://www.theverge.com/rss/frontpage';
-            return callUrl(url, 'entry');
+            return getFeed(url, 'entry');
         },
         getHackerNews: function () {
             var url = 'https://news.ycombinator.com/rss';
-            return callUrl(url, 'item');
+            return getFeed(url, 'item');
         }
     };
 })
